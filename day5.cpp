f7de5c2118b6d9f3702c3f30b0ac6be08bc685bf
Adam-42 <aa.dobson@yahoo.co.uk>
2021-12-04 06:21:36 +0000

    Implemented solutions for Day 4. Created template for Day 5.

---
+++ day5.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+enum class Direction
+{
+	forward,
+	down,
+	up,
+};
+
+struct Move
+{
+	const Direction direction;
+	const int		magnitude;
+};
+
+struct SubState
+{
+	int64_t pos;
+	int64_t depth;
+	int64_t aim;
+};
+
+constexpr Move g_data1[] = {{Direction::forward, 2}};
+
+constexpr Move g_dataTest[] = {
+	{Direction::forward, 5}, {Direction::down, 5}, {Direction::forward, 8}, {Direction::up, 3}, {Direction::down, 8}, {Direction::forward, 2},
+};
+
+static int64_t Task1(const Move data[], size_t length)
+{
+	SubState result = {};
+
+	for (size_t i = 0; i < length; i++)
+	{
+		switch (data[i].direction)
+		{
+			case Direction::forward:
+				result.pos += data[i].magnitude;
+				break;
+			case Direction::down:
+				result.depth += data[i].magnitude;
+				break;
+			case Direction::up:
+				result.depth -= data[i].magnitude;
+				break;
+		}
+	}
+
+	return result.pos * result.depth;
+}
+
+static int64_t Task2(const Move data[], size_t length)
+{
+	SubState result = {};
+
+	for (size_t i = 0; i < length; i++)
+	{
+		switch (data[i].direction)
+		{
+			case Direction::forward:
+				result.pos += data[i].magnitude;
+				result.depth += result.aim * data[i].magnitude;
+				break;
+			case Direction::down:
+				result.aim += data[i].magnitude;
+				break;
+			case Direction::up:
+				result.aim -= data[i].magnitude;
+				break;
+		}
+	}
+
+	return result.pos * result.depth;
+}
+
+void Day5()
+{
+	//std::cout << "From test1, combined position was " << Task1(g_dataTest, std::size(g_dataTest)) << "\n";
+
+	//std::cout << "From task1, combined position was " << Task1(g_data1, std::size(g_data1)) << "\n";
+
+	//std::cout << "From test2, combined position was " << Task2(g_dataTest, std::size(g_dataTest)) << "\n";
+
+	//std::cout << "From task2, combined position was " << Task2(g_data1, std::size(g_data1)) << "\n";
+}
--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@ extern void Day1();
 extern void Day2();
 extern void Day3();
 extern void Day4();
+extern void Day5();
+extern void Day6();
+extern void Day7();
+extern void Day8();
+extern void Day9();
+extern void Day10();
+extern void Day11();
+extern void Day12();
+extern void Day13();
+extern void Day14();
+extern void Day15();
+extern void Day16();
+extern void Day17();
+extern void Day18();
+extern void Day19();
+extern void Day20();
+extern void Day21();
+extern void Day22();
+extern void Day23();
+extern void Day24();
+extern void Day25();
 
 int main(int argc, char** argv)
 {
@@ -18,4 +39,7 @@ int main(int argc, char** argv)
 
 	std::cout << "Day4\n----\n";
 	Day4();
+
+	std::cout << "Day5\n----\n";
+	Day5();
 }
